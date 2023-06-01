e6febfa1bc251bf86ff8a57d381a82b7707262ca
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2023-06-01 19:51:01 -0300

    Use random number generation to generate answer at runtime
    Signed-off-by: Renan Augusto Fengler <renan_fengler_93@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
+    let answer: i32 = rand::thread_rng().gen_range(1..=100);
+
     println!("Guess the number");
     println!("Plase input your guess");
+    println!("number {answer}");
 
     let mut guess: String = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
