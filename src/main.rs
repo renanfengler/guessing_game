ab64234b54f987a5f54d78435d9b5c54bbaec55a
Renan Augusto Fengler <renan_fengler_93@hotmail.com>
2023-06-04 06:17:08 -0300

    Parse guess to u32 and comparing to to secret_number
    Signed-off-by: Renan Augusto Fengler <renan_fengler_93@hotmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
-use std::io;
+use std::{io, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
-    let answer: i32 = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Guess the number");
     println!("Plase input your guess");
-    println!("number {answer}");
+    println!("number {secret_number}");
 
-    let mut guess: String = String::new();
+    let mut guess  = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
